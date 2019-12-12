c721310ab5b1913ceb3baa6ffa7eed4739108125
Joao Guilherme Pan <joaoguilhermepan@gmail.com>
2019-12-12 17:04:09 +0000

    added missing dependencies

--- projects/ngx-mat-simple-autocomplete/src/lib/ngx-mat-simple-autocomplete.module.ts
+++ projects/ngx-mat-simple-autocomplete/src/lib/ngx-mat-simple-autocomplete.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { NgxMatSimpleAutocompleteComponent } from './ngx-mat-simple-autocomplete.component';
+import { MaterialModule } from 'src/config/material.module';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [NgxMatSimpleAutocompleteComponent],
   imports: [
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   exports: [NgxMatSimpleAutocompleteComponent]
 })
--- projects/ngx-mat-simple-autocomplete/src/public-api.ts
+++ projects/ngx-mat-simple-autocomplete/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-mat-simple-autocomplete
  */
 
-export * from './lib/ngx-mat-simple-autocomplete.service';
 export * from './lib/ngx-mat-simple-autocomplete.component';
 export * from './lib/ngx-mat-simple-autocomplete.module';
